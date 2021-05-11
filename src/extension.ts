98dbd3ab52a08eede11d2f716e2591331e1f9179
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-11 19:40:31 +0530

    Basic files

---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('tictactoe.start', () => {
+		// Create and show a new webview
+		const panel = vscode.window.createWebviewPanel(
+			'tictactoe', // Identifies the type of the webview. Used internally
+			'TicTacToe', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
+		// panel.webview.html = getWebviewContent();
+		const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'src', 'game.html'));
+		panel.webview.html = fs.readFileSync(filePath.fsPath, 'utf8');
+	});
+	context.subscriptions.push(disposable);
+}
